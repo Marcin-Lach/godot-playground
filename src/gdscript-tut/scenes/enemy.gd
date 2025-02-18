3a1d2335d3df80e22a64af0b061729fd5528863d
Marcin Lach <marcin.lach.90@gmail.com>
2025-02-18 22:08:26 +0100

    added movement to the enemy through script
    enemy will bounce between walls

--- src/gdscript-tut/scenes/enemy.gd
+++ src/gdscript-tut/scenes/enemy.gd
@@ -1,12 +1,22 @@
 extends Node2D
 
 # https://youtu.be/LOhfqjmasi0?si=8jyx_ljiL9lQ0qaq&t=2640
+const SPEED : int = 60
+var direction = 1
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
+@onready var ray_cast_right: RayCast2D = $RayCastRight
+@onready var ray_cast_left: RayCast2D = $RayCastLeft
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	if ray_cast_right.is_colliding():
+		animated_sprite.flip_h = true
+		direction = -1
+		
+	if ray_cast_left.is_colliding():
+		animated_sprite.flip_h = false
+		direction = 1
+	
+	position.x += direction * SPEED * delta
 	pass
--- src/gdscript-tut/scenes/killzone.gd
+++ src/gdscript-tut/scenes/killzone.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+# https://youtu.be/LOhfqjmasi0?si=4J6gHdkY24banGz_&t=3068
+
 @onready var timer: Timer = $Timer
 
 func _on_body_entered(body: Node2D) -> void:
